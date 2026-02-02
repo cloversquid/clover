1884ad2682ffd13dd8c8985c3fbd18d6d580fe93
Clover <francisolivialee@gmail.com>
2026-02-02 11:41:12 -0800

    update homework files

---
+++ homework2/lecturenotesweek3.py
@@ -0,0 +1,14 @@
+'''
+pwd = print working directory 
+ls = list files and directories
+cd = change directories
+mkdir = make a new directory
+nano. = opens a text file editor
+
+string = text/language
+integer = whole number
+float = number with decimal
+
+call w/ python3 <script.py> or python3 and then type code directly into command line
+'''
+
